18a7a9a226ef70b6e511061b9ff14db93332712b
girish97115 <kiira97115@gmail.com>
2021-02-17 08:47:09 +0530

    removed mail feature

--- models.py
+++ models.py
@@ -172,30 +172,30 @@ class DocumentModel(db.Model):
     def update_db(self):
         db.session.commit()
 
-class InviteModel(db.Model):
-    __tablename__ = 'invite'
+# class InviteModel(db.Model):
+#     __tablename__ = 'invite'
 
-    id = db.Column(db.Integer, primary_key=True)
-    mail = db.Column(db.String(120), nullable=False)
-    team_id = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True),
-                           server_default=func.now())
-    updated_at = db.Column(db.DateTime(timezone=True),
-                           server_default=func.now(), onupdate=func.now())
+#     id = db.Column(db.Integer, primary_key=True)
+#     mail = db.Column(db.String(120), nullable=False)
+#     team_id = db.Column(db.Integer, nullable=False)
+#     created_at = db.Column(db.DateTime(timezone=True),
+#                            server_default=func.now())
+#     updated_at = db.Column(db.DateTime(timezone=True),
+#                            server_default=func.now(), onupdate=func.now())
 
-    def __repr__(self):
-        return '<Invite model {}, {}>'.format(self.id, self.mail)
+#     def __repr__(self):
+#         return '<Invite model {}, {}>'.format(self.id, self.mail)
 
-    def __init__(self, mail, team_id):
-        self.mail = mail
-        self.team_id = team_id
+#     def __init__(self, mail, team_id):
+#         self.mail = mail
+#         self.team_id = team_id
 
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+#     def save_to_db(self):
+#         db.session.add(self)
+#         db.session.commit()
 
-    def update_db(self):
-        db.session.commit()
+#     def update_db(self):
+#         db.session.commit()
 
 
 class UsersSchema(ModelSchema):
--- resources/InviteResources.py
+++ resources/InviteResources.py
@@ -1,43 +1,43 @@
-from flask_restful import Resource, reqparse
-from models import InviteModel, TeamModel
-from marshmallow_sqlalchemy import ModelSchema
-from flask_jwt_extended import (create_access_token, create_refresh_token,
-                                jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt, set_access_cookies, set_refresh_cookies, unset_jwt_cookies)
-from flask import jsonify
-from run import mail
-from flask_mail import Message
+# from flask_restful import Resource, reqparse
+# from models import InviteModel, TeamModel
+# from marshmallow_sqlalchemy import ModelSchema
+# from flask_jwt_extended import (create_access_token, create_refresh_token,
+#                                 jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt, set_access_cookies, set_refresh_cookies, unset_jwt_cookies)
+# from flask import jsonify
+# from run import mail
+# from flask_mail import Message
 
-invite_parser = reqparse.RequestParser()
-invite_parser.add_argument(
-    'email', help='This field cannot be blank', required=True)
-invite_parser.add_argument(
-    'team_id', help='This field cannot be blank', required=True)
-
-
-class InviteSchema(ModelSchema):
-    class Meta:
-        model = InviteModel
+# invite_parser = reqparse.RequestParser()
+# invite_parser.add_argument(
+#     'email', help='This field cannot be blank', required=True)
+# invite_parser.add_argument(
+#     'team_id', help='This field cannot be blank', required=True)
 
 
-invite_schema = InviteSchema(
-    only=("email", "team_id"))
-invites_schema = InviteSchema(
-    only=("email", "team_id"), many=True)
+# class InviteSchema(ModelSchema):
+#     class Meta:
+#         model = InviteModel
 
 
-class AdminSendInvite(Resource):
-    def post(self):
-        args = invite_parser.parse_args()
-        invite = InviteModel(args['email'], args['team_id'])
-        team = TeamModel.query.get(args['team_id'])
-        invite.save_to_db()
-        try:
-            msg = Message("Alert From Taskify",
-                          sender="Taskify@gmail.com",
-                          recipients=[args['email']])
-            msg.body = "You have been invited to team {} , please sign in to taskify and start Collaborating".format(
-                team.name)
-            mail.send(msg)
-            return invite_schema.dump(invite)
-        except Exception as e:
-            return {'message': 'Something went wrong'}, 500
+# invite_schema = InviteSchema(
+#     only=("email", "team_id"))
+# invites_schema = InviteSchema(
+#     only=("email", "team_id"), many=True)
+
+
+# class AdminSendInvite(Resource):
+#     def post(self):
+#         args = invite_parser.parse_args()
+#         invite = InviteModel(args['email'], args['team_id'])
+#         team = TeamModel.query.get(args['team_id'])
+#         invite.save_to_db()
+#         try:
+#             msg = Message("Alert From Taskify",
+#                           sender="Taskify@gmail.com",
+#                           recipients=[args['email']])
+#             msg.body = "You have been invited to team {} , please sign in to taskify and start Collaborating".format(
+#                 team.name)
+#             mail.send(msg)
+#             return invite_schema.dump(invite)
+#         except Exception as e:
+#             return {'message': 'Something went wrong'}, 500
